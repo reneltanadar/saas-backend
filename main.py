ea8e723029cb8ca60d431a4da45450d27850b168
reneltanadar <reneltanadar@gmail.com>
2026-06-05 07:24:39 -0500

    day 2:path params,query params, error handling

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status
 
 app=FastAPI()
-users=[]
+users=[
+ {
+    "id":1,"name":"John","email":"john@gmail.com"
+ },
+ {
+     "id":2,"name":"Allie","email":"allie@gmail.com"
+ }
+]
 
 @app.get("/")
 async def home():
     return{
-        "message": "Hello "
+        "message": "SAAS Backend is Running "
     }
 
 @app.get("/users")
@@ -15,8 +22,34 @@ async def get_users():
         "Users": users
     }
 
-@app.post("/users")
+@app.get("/users/search")
+async def search_users(name: str =None,limit:int=10):
+    results=users
+
+    if name:
+        results=[u for u in users if name.lower() in u["name"].lower() ]
+
+    return{
+        "users": results[:limit],
+        "count":len(results[:limit])
+    }
+
+@app.get("/users/{user_id}")
+async def get_user(user_id:int):
+    for user in users:
+        if user["id"]==user_id:
+
+            return{
+        "user": user
+                }
+        
+    raise HTTPException(status_code=404,detail="User Not Found")
+
+@app.post("/users",status_code=status.HTTP_201_CREATED)
 async def create_users(user :dict):
+    for existing in users:
+        if existing["email"]==user.get("email"):
+            raise HTTPException(status_code=404,detail="Email already Exists")
     users.append(user)
     return{
         "Message": "User Created",
